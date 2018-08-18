a6876e86ebde203659528b9031efc46b713ceae7
Zorance <zorance@yeah.net>
2018-08-18 11:09:48 +0800

    Vue_Project upload to the Git

---
+++ src/components/toDoItems.vue
@@ -0,0 +1,19 @@
+<template>
+    <li @click="del" :id="index">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props:['index','content'],
+  methods:{
+      del(){
+        this.$emit('remove',this.index);
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/todoList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+     <input v-model="val" />
+     <button @click="submitItem">提交</button>
+     <ul>
+        <todo-items 
+          v-for="(item,index) of list" 
+          :key="index"
+          :content="item"
+          :index="index"
+          @remove="remove">
+        </todo-items>
+     </ul>
+  </div>
+</template>
+
+<script>
+import toDoItems from './components/toDoItems'
+
+export default {
+  components:{
+      'todo-items':toDoItems
+  },
+  data(){
+    return {
+        val:'',
+        list:[]
+    }
+  },
+  methods:{
+    submitItem(){
+      this.list.push(this.val);
+      this.val='';
+    },
+    remove(index){
+      this.list.splice(index,1);
+    }
+  }
+}
+</script>
+
+<style>
+
+
+</style>
